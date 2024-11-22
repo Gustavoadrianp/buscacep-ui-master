ff23a9c764c91cb826b7c863db14ee4feaa097de
Gustavoadrianp <133925800+Gustavoadrianp@users.noreply.github.com>
2024-11-21 23:02:31 -0300

    Implementação CNPJ

--- src/app/cnpj/cnpj.component.ts
+++ src/app/cnpj/cnpj.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { MessageService } from 'primeng/api';
+import { CnpjService } from './cnpj.service';
 
 @Component({
   selector: 'app-cnpj',
@@ -6,10 +9,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cnpj.component.css']
 })
 export class CnpjComponent implements OnInit {
-
-  constructor() { }
+  @Input() titleHome = 'Consultando CNPJ';
+  buscaCNPJ: string = '';
+  buscar: boolean = false;
+  constructor(
+    private cnpjService: CnpjService,
+    private messageService: MessageService,
+    private title: Title
+  ) {}
 
   ngOnInit() {
+    this.title.setTitle('Buscando CNPJ');
   }
 
+  buscarCNPJ(buscaCNPJ: any, form: any) {
+    if (buscaCNPJ && buscaCNPJ.length >= 14) {
+      this.cnpjService.consultaCNPJ(buscaCNPJ).subscribe({
+        next: (dados: any) => {
+          this.buscar = true;
+          setTimeout(() => {
+            this.populaCNPJForm(dados, form);
+            console.log('Dados recebidos com sucesso');
+          }, 100);
+        },
+        error: (e: any) => {
+          this.resetaCNPJForm(form);
+          this.buscar = false;
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Atenção',
+            detail: 'Erro ao buscar CNPJ!'
+          });
+        }
+      });
+    } else {
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Atenção',
+        detail: 'CNPJ inválido ou incompleto!'
+      });
+    }
+  }
+
+  populaCNPJForm(dados: any, formulario: any) {
+    formulario.form.patchValue({
+      razaoSocial: dados.razao_social,
+      nomeFantasia: dados.nome_fantasia,
+      endereco: dados.logradouro,
+      cidade: dados.municipio,
+      estado: dados.uf
+    });
+  }
+
+  resetaCNPJForm(formulario: any) {
+    formulario.form.patchValue({
+      razaoSocial: null,
+      nomeFantasia: null,
+      endereco: null,
+      cidade: null,
+      estado: null
+    });
+    this.buscar = false;
+  }
 }
